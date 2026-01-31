2812765a2647a3f0e405b22861f19c28aa82c523
TheRonnieSouza <ronniesouza.contato@gmail.com>
2026-01-30 22:41:12 -0300

    feat: add vector db qdrant

--- app/main.py
+++ app/main.py
@@ -9,16 +9,32 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 #from typing_extensions import List, TypedDict
+from qdrant_client.models import Distance, VectorParams
+from langchain_qdrant import QdrantVectorStore
+from qdrant_client import QdrantClient, models
 
 from fastapi import FastAPI, File , UploadFile 
 from typing import Annotated
 import pymupdf
+from uuid import uuid4
 from app.config.settings import Settings
+
+from langchain_core.documents import Document
+
 
 
 app = FastAPI()
 
 settings = Settings()
+
+print(f"qdrant connection: {settings.qdrant.connection}")
+client = QdrantClient(url=settings.qdrant.connection)
+
+if not client.collection_exists("documents"):
+    client.create_collection(
+        collection_name="documents",
+        vectors_config=models.VectorParams(size=3072, distance=models.Distance.COSINE)
+    )
 
 @app.get("/")
 async def read_root():
@@ -29,6 +45,68 @@ async def ingest_document():
     
     embeddings = GoogleGenerativeAIEmbeddings(model=settings.google.embedding_model_name, api_key=settings.google.embedding_api_key)
     vector = embeddings.embed_query("hello, world!")
+    
+    vector_store = QdrantVectorStore(
+    client=client,
+    collection_name="documents",
+    embedding=embeddings,
+)
+    document_1 = Document(
+    page_content="I had chocolate chip pancakes and scrambled eggs for breakfast this morning.",
+    metadata={"source": "tweet"},
+    )
+    document_2 = Document(
+    page_content="The weather forecast for tomorrow is cloudy and overcast, with a high of 62 degrees Fahrenheit.",
+    metadata={"source": "news"},
+    )
+    document_3 = Document(
+    page_content="Building an exciting new project with LangChain - come check it out!",
+    metadata={"source": "tweet"},
+    )
+    document_4 = Document(
+    page_content="Robbers broke into the city bank and stole $1 million in cash.",
+    metadata={"source": "news"},
+    )
+    document_5 = Document(
+    page_content="Wow! That was an amazing movie. I can't wait to see it again.",
+    metadata={"source": "tweet"},
+    )
+    document_6 = Document(
+    page_content="Is the new iPhone worth the price? Read this review to find out.",
+    metadata={"source": "website"},
+    )
+    document_7 = Document(
+    page_content="The top 10 soccer players in the world right now.",
+    metadata={"source": "website"},
+    )
+    document_8 = Document(
+    page_content="LangGraph is the best framework for building stateful, agentic applications!",
+    metadata={"source": "tweet"},
+    )  
+    document_9 = Document(
+    page_content="The stock market is down 500 points today due to fears of a recession.",
+    metadata={"source": "news"},
+    )
+    document_10 = Document(
+    page_content="I have a bad feeling I am going to get deleted :(",
+    metadata={"source": "tweet"},
+    )
+    documents = [
+    document_1,
+    document_2,
+    document_3,
+    document_4,
+    document_5,
+    document_6,
+    document_7,
+    document_8,
+    document_9,
+    document_10,
+    ]
+    uuids = [str(uuid4()) for _ in range(len(documents))]
+        
+    vector_store.add_documents(documents=documents, ids=uuids)
+    
     vector[:5]
     print(f'vectores: {vector[0]}')
     
